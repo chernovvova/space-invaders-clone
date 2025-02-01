25168f30182c42bb2d28b9770d3e35d60950ffcc
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-02-01 13:58:36 +0300

    feat: add new imports

--- loader.lua
+++ loader.lua
@@ -4,17 +4,26 @@ function loadFiles()
 end
 
 function loadLibraries()
-    Object = require './lua/libraries/classic/classic'
+    Object = require 'lua/libraries/classic/classic'
+    Timer = require 'lua/libraries/hump/timer'
+    Gamestate = require 'lua/libraries/hump/gamestate'
 end
 
 function loadObjects()
-    objects = {}
+    local objects = {}
     getFolderFilesRecursively('lua/objects', objects)
     requireFiles(objects)
+end
+
+function loadGameStates()
+    require 'lua/states/pause'
+    require 'lua/states/game'
+    require 'lua/states/menu'
 end
 
 function loadGame()
     loadLibraries()
     loadFiles()
     loadObjects()
+    loadGameStates()
 end
