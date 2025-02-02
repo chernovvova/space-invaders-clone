3fc7a57c9e773e5459f7df7ec9e42d391b78686c
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-02-02 22:31:25 +0300

    feat: add player collision

--- lua/objects/player.lua
+++ lua/objects/player.lua
@@ -1,7 +1,26 @@
 Player = GameObject:extend()
 
-function Player:new(x, y)
+function Player:new(game, x, y)
     Player.super.new(self, x, y)
+
+    self.width = 32
+    self.height = 32
+    self.spriteScaleX = 5
+    self.spriteScaleY = 5
+
+    local body = love.physics.newBody(
+        game.world, 
+        self.x, 
+        self.y, 
+        'dynamic'
+    )
+    local shape = love.physics.newRectangleShape(
+        self.x + self.width * self.spriteScaleX / 4, 
+        self.y + self.height * self.spriteScaleY / 4, 
+        self.width * self.spriteScaleX / 2, 
+        self.height * self.spriteScaleY / 2
+    )
+    self.fixture = love.physics.newFixture(body, shape)
 
     self.velocity = 200
     self.sprite = love.graphics.newImage('assets/sprites/player.png')
@@ -9,13 +28,28 @@ end
 
 function Player:update(dt)
     if love.keyboard.isDown('right') then
-        self.x = self.x + self.velocity * dt
-    end
-    if love.keyboard.isDown('left') then
-        self.x = self.x - self.velocity * dt
+        self.fixture:getBody():setLinearVelocity(self.velocity, 0)
+    elseif love.keyboard.isDown('left') then
+        self.fixture:getBody():setLinearVelocity(-self.velocity, 0)
+    else
+        self.fixture:getBody():setLinearVelocity(0, 0)
     end
 end
 
 function Player:draw()
-    love.graphics.draw(self.sprite, self.x, self.y, nil, 5, 5)
+    love.graphics.draw(
+        self.sprite, 
+        self.fixture:getBody():getX(),
+        self.fixture:getBody():getY(),
+        nil,
+        self.spriteScaleX,
+        self.spriteScaleY
+    )
+    love.graphics.rectangle(
+        'line', 
+        self.fixture:getBody():getX() + self.width * self.spriteScaleX / 4,
+        self.fixture:getBody():getY() + self.height * self.spriteScaleY / 4, 
+        self.width * self.spriteScaleX / 2, 
+        self.height * self.spriteScaleY / 2
+    )
 end
--- lua/states/game.lua
+++ lua/states/game.lua
@@ -1,11 +1,18 @@
 GameState = Object:extend()
 
 function GameState:init()
-    self.player = Player(love.graphics.getWidth() / 2 - 64, love.graphics.getHeight() / 2 - 64)
-    print(self.palyer)
+    self.world = love.physics.newWorld(0, 0, true)
+    self.player = Player(
+        self, 
+        love.graphics.getWidth() / 2 - 64, 
+        love.graphics.getHeight() / 2 - 64
+    )
 end
 
 function GameState:update(dt)
+    if self.world then
+        self.world:update(dt)
+    end
     if self.player then
         self.player:update(dt)
     end
